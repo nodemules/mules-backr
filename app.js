9f06c4b6dc10bc130b96923f402f7b0b5361b407
staticsound <johngreedjr@gmail.com>
2017-09-21 21:56:16 -0400

    First commit

---
+++ app.js
@@ -0,0 +1,33 @@
+{
+  const backup = require('s3-mongo-backup');
+  const config = require('./config.js');
+  const CronJob = require('cron').CronJob;
+  const db = config.mongo;
+  const bucket = config.bucket;
+
+  console.log('Starting Mules Backup Service');
+
+  const backupConfig = {
+    mongodb: `mongodb://${db.user}:${db.key}@${db.host}:${db.port}/${db.name}`, // MongoDB Connection URI
+    s3: {
+      accessKey: bucket.accessKey, //AccessKey
+      secretKey: bucket.secretKey, //SecretKey
+      region: bucket.region, //S3 Bucket Region
+      accessPerm: 'private', //S3 Bucket Privacy, Since, You'll be storing Database, Private is HIGHLY Recommended
+      bucketName: bucket.name //Bucket Name
+    },
+    keepLocalBackups: false,
+    timezoneOffset: config.timezoneOffset
+  };
+
+  new CronJob(config.backupTime, function() {
+    backup(backupConfig).then(() => {
+      console.log('Successful Backup for ' + new Date());
+    }, (err) => {
+      console.log('There was an error backing up data on ' + new Date());
+      console.log(err);
+    });
+
+  }, null, true, 'America/New_York');
+
+}
---
+++ config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  mongo: {
+    user: '',
+    key: '',
+    host: '',
+    port: '',
+    name: ''
+  },
+  bucket: {
+    accessKey: '',
+    secretKey: '',
+    region: '',
+    name: ''
+  },
+  timezoneOffset: -4,
+  backupTime: '0 0 1 * * *'
+};
